3b19dfc483feaf9544059054c17a6c3df7f5694c
MSM <msm74588@gmail.com>
2023-07-05 13:25:44 +0530

    sudo check added

--- live-build.sh
+++ live-build.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+
+set -e
+
+# check for root permissions
+if [[ "$(id -u)" != 0 ]]; then
+  echo "E: Requires root permissions" > /dev/stderr
+  exit 1
+fi
+
 # install dependencies
 apt-get update
 apt-get install -y live-build gnupg2 binutils zstd ca-certificates
+
+apt-get install apt install squashfs-tools live-boot live-config live-boot-initramfs-tools live-config-sysvinit libburn4 libisofs6 libisoburn1 xorriso isolinux 
 
 ARCH="amd64"
 BASECODENAME="sid"
