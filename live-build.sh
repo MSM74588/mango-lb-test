d378ef5a652d1d11c1b1cf7db21b3dd8ff8257dc
Spike_Projects <msm74588@gmail.com>
2023-07-15 08:36:27 +0530

    removed auto/build

--- live-build.sh
+++ live-build.sh
@@ -8,5 +8,25 @@ if [[ "$(id -u)" != 0 ]]; then
   echo "E: Requires root permissions" > /dev/stderr
   exit 1
 fi
- 
+
+apt-get update
+
+apt-get install -y \
+    live-build \
+    gnupg2 \
+    binutils \
+    zstd \
+    ca-certificates \
+    squashfs-tools  \
+    live-boot  \
+    live-config  \
+    live-boot-initramfs-tools \
+    live-config-sysvinit \
+    libburn4  \
+    libisofs6  \
+    libisoburn1  \
+    xorriso \
+    curl
+
+
 sudo lb build
