2565c77a42139161d8dc698aaa2ba78e9e6fe4a5
Spike_Projects <msm74588@gmail.com>
2023-07-15 23:35:51 +0530

    added gnome extensions

--- config/hooks/chroot/10-install-gnome-extensions.sh
+++ config/hooks/chroot/10-install-gnome-extensions.sh
@@ -6,6 +6,12 @@
 # - https://extensions.gnome.org/extension/3088/extension-list/
 # test
 
+# 307 - Dash to dock - https://extensions.gnome.org/extension/307/dash-to-dock/ > dash-to-dock@micxgx.gmail.com
+# 4337 -Desktop-Icons-NEO - https://extensions.gnome.org/extension/4337/desktop-icons-neo/ > desktopicons-neo@darkdemon
+# 3210 - Compiz Window Effect - https://extensions.gnome.org/extension/3210/compiz-windows-effect/ > compiz-windows-effect@hermes83.github.com
+# 3193 - Blur My Shell - https://extensions.gnome.org/extension/3193/blur-my-shell/ > blur-my-shell@aunetx
+# https://extensions.gnome.org/extension/517/caffeine/
+
 set -e
 
-# chroot "$CHROOTDIR" /hooks/gnome-extensions-installer 3088
+chroot "$CHROOTDIR" /hooks/gnome-extensions-installer 307 4337 3210 3193 517
